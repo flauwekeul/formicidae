afa4c9f876e471d6ddff33b6d2d0a2f93b59039b
Abbe Keultjes <flauwekeul@gmail.com>
2021-07-20 13:41:04 +0200

    initial commit with typescript, prettier, eslint, stylelint, snowpack and lint-staged

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:json/recommended-with-comments',
+    'prettier',
+  ],
+}
