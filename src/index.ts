a7650d42384a0a097596d85acdc32493241eb920
Abbe Keultjes <flauwekeul@gmail.com>
2021-08-20 16:13:09 +0200

    transition moving and rotating ants

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { createHexPrototype, Grid } from 'honeycomb-grid'
 import { Ant } from './ant'
-import { TILE_SIZE } from './setting'
+import { ANT_TRANSITION_DURATION_IN_MS, TILE_SIZE } from './setting'
 import { Tile } from './types'
 
+// setup
 const hexPrototype = createHexPrototype<Tile>({ dimensions: { width: TILE_SIZE, height: TILE_SIZE } })
 const grid = new Grid(hexPrototype)
+document.documentElement.style.setProperty('--ant-transition-duration', `${ANT_TRANSITION_DURATION_IN_MS}ms`)
 
 const startTile = grid.getHex([20, 20])
 const ant = new Ant(grid, startTile, 90)
--- src/setting.ts
+++ src/setting.ts
@@ -1,2 +1,3 @@
 export const TILE_SIZE = 30
 export const ANT_SIZE_MODIFIER = 1.3
+export const ANT_TRANSITION_DURATION_IN_MS = 200
